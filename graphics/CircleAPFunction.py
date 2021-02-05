681fbb6a933f9cd349a0506b31da746c2dbfecfc
Gopika Sabu <76676531+gopika988@users.noreply.github.com>
2021-02-05 09:20:47 +0530

    Add files via upload

---
+++ graphics/CircleAPFunction.py
@@ -0,0 +1,9 @@
+#circle area
+def CArea(r):
+    result=3.14*r*r
+    return result
+
+#circle perimeter
+def CPerimetr(r):
+    result=2*3.14*r
+    return result
---
+++ graphics/dgraphics/cuboidAPFunction.py
@@ -0,0 +1,9 @@
+#area of cuboid
+def Acuboid(a):
+    result=6*a*a
+    return result
+
+#perimeter of cuboid
+def Pcuboid(l,b,h):
+    result=4*(l+b+h)
+    return result
